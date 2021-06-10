01295c4c94f4d1cc983d4f4426942f7c72f9cc37
JCKH <55143530+jckh-dev@users.noreply.github.com>
2021-06-10 19:15:01 +1000

    index.js complete, user.js basic login and register functions complete, profile routes underway

--- routes/index.js
+++ routes/index.js
@@ -1,60 +1,79 @@
 const express = require('express');
 const router = express.Router();
-
+const jwt = require('jsonwebtoken');
 // helper functions
 
-function checkYearFormat(year) {
-  var checkYear = !!(+year == year && year.length === 4 && year.trim() == year);
+
+// These three functions takes the user input for the year/country/limit paramater and checks to see if the input is of a valid format for that input type.
+function checkYearFormat(param) {
+  var checkYear = !!(+param == param && param.length === 4 && param.trim() == param);
+  // Returns true if format is valid
+  // Returns false if format is invalid
   return checkYear;
+};
+
+function checkCountryFormat(param) {
+  const regexp = /\d/g;
+  var checkCountry = regexp.test(param);
+  // returns true if a number is detected (invalid)
+  // returns false if no number is detected (valid)
+  return checkCountry
+};
+
+function checkLimitFormat(param) {
+  const regexp = /^[1-9]\d*$/;
+  var checkLimit = regexp.test(param);
+  // returns true if format is valid
+  // returns false if non-integer number is detected (invalid)
+  return checkLimit
 }
 
 
-function checkRowLength(res, rows, params) {
-
-  if (params) {
-    const keys = Object.keys(params);
-    const values = Object.values(params);
-
-    const key1 = keys[0];
-    const key2 = keys[1];
-    const param1 = values[0];
-    const param2 = values[1];
-  }
-  if (!rows.length) {
-    !key2 ? (res.json({ "Error": false, "Message": "No records matching your search query: \' " + key1 + " : " + param1 + " \'. " })) : (res.json({ "Error": false, "Message": "No records matching your search query \' " + key1 + " : " + param1 + " \' and \' " + key2 + " : " + param2 + " \'." }))
-  } else {
-    res.json({ "Error": false, "Message": "Success", "Query Results ": rows })
-  }
-
-}
-
-
-const withOptionalRankParams = function (queryBuilder, params) {
+// These two functions takes an array of query parameters and destructures the values into the valid keys for that query and all remaining keys. It then checks to see if there are any 'remaining'.
+// Returns true if there are any extra invalid parameter keys.
+// Returns false if there are no invalid parameters. 
+function checkInvalidRankParams(params) {
 
   const { year, country, ...invalidRemaining } = params;
-  invalidKeys = Object.keys(invalidRemaining);
+  var invalidKeys = Object.keys(invalidRemaining);
 
-  const keys = Object.keys(params);
-  const values = Object.values(params);
+  console.log(invalidKeys);
 
-  const key1 = keys[0];
-  const key2 = keys[1];
-  const param1 = values[0];
-  const param2 = values[1];
-
-  if (invalidKeys) {
-    res.status(400).json({ "Error": true, "Message": "Invalid Query Parameters. Only year and country are permitted." })
+  if (invalidKeys.length > 0) {
+    return true;
+  } else {
+    return false;
   }
-  if (param1 && param2) {
-    queryBuilder.where(key1, param1).andWhere(key2, param2);
+}
+
+function checkInvalidFactParams(params) {
+
+  const { limit, country, ...invalidRemaining } = params;
+  var invalidKeys = Object.keys(invalidRemaining);
+
+  if (invalidKeys.length > 0) {
+    return true;
+  } else {
+    return false;
   }
-  if (param1 && !param2) {
-    queryBuilder.where(key1, param1);
+}
+
+// function is a query builder for the rankings endpoint knex db query. Takes the optional year and country values, checks to see if they are defined or undefined, and associates the required query structure accordingly.
+const withOptionalRankParams = function (queryBuilder, yearVal, countryVal) {
+
+  if (yearVal && countryVal) {
+    queryBuilder.where('year', yearVal).andWhere('country', countryVal);
+  }
+  else if (yearVal && !countryVal) {
+    queryBuilder.where('year', yearVal);
+  }
+  else if (!yearVal && countryVal) {
+    queryBuilder.where('country', countryVal)
   }
 
 };
 
-
+// function is a query builder for the factors endpoint knex db query. Takes the optional year and limit values, checks to see if they are defined or undefined, and associates the required query structure accordingly.
 const withOptionalFactorParams = function (queryBuilder, params) {
 
   let country = params.country;
@@ -81,94 +100,145 @@ router.get('/', function (req, res, next) {
 // GET rankings Page
 router.get("/rankings", function (req, res, next) {
 
-  // destructure req.query then do a check to see if invalidRemaining holds any values and , if so, throw invalid response, 
-  // const { year, country, ...invalidRemaining } = req.query;
-  // invalidKeys = Object.keys(invalidRemaining);
+  const optionalRankParams = req.query;
+  const yearVal = req.query.year;
+  const countryVal = req.query.country;
 
-  // old param variable.... pending delete
-  optionalRankParams = req.query;
+  // running variable parameters through suitable checks, returning boolean values
+  var invalidParams = checkInvalidRankParams(optionalRankParams);
+  var yearFormat = checkYearFormat(yearVal);
+  var countryFormat = checkCountryFormat(countryVal);
 
-  req.db.from('rankings')
-    .select('rank', 'country', 'score', 'year')
-    .modify(withOptionalRankParams, optionalRankParams)
-    .orderBy('year', 'desc')
-    .then((rows) => {
-      checkRowLength(res, rows, optionalRankParams)
-    })
-    .catch((err) => {
-      console.log(err);
-      res.json({ "Error": true, "Message": "Invalid query parameters. Query parameters are not permitted." })
-    })
+  // test boolean values of parameter check results to sanitise DB query values. if a test is true, throw status error with suitable message. if 
+  if (invalidParams) {
+    res.status(400).json({ "Error": true, "Message": "Invalid Query Parameters. Only year and country are permitted." })
+    return
+  }
+  else if (yearVal && !yearFormat) {
+    res.status(400).json({ "Error": true, "Message": "Invalid year format. Format must be yyyy." })
+    return
+  }
+  else if (countryFormat) {
+    res.status(400).json({ "Error": true, "Message": "Country query parameter cannot contain numbers." })
+    return
+  }
+  // if all checks return false, continue with DB query construction
+  else {
+    req.db.from('rankings')
+      .select('rank', 'country', 'score', 'year')
+      // The query builder will take both optional params in but filter out any undefined values to produce final db query 'where' statements.
+      .modify(withOptionalRankParams, yearVal, countryVal)
+      .orderBy('year', 'desc')
+      // display successful results. will return empty array if no results.
+      .then((rows) => {
+        res.status(200).json(rows)
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json({ "Error": true, "Message": "Error executing MySQL query" })
+      })
+  }
 });
 
+
+// GET countries route
 router.get("/countries", function (req, res, next) {
-  req.db.from('rankings')
-    .select('country')
-    .distinct()
-    .orderBy('country', 'ASC')
-    .then((rows) => {
-      checkRowLength(res, rows)
-    })
-    .catch((err) => {
-      console.log(err);
-      res.json({ "Error": true, "Message": "Invalid query parameters. Query parameters are not permitted." })
-    })
+
+  // capture any additional query parameters submitted with the GET request
+  const queryParams = req.query;
+
+  // if there are any parameters in the request, throw error with suitable message.
+  if (queryParams.length > 0) {
+    res.status(400).json({ "Error": true, "Message": "Invalid Query Parameters. Query parameters are not permitted." })
+    return
+  }
+  else {
+    req.db.from('rankings')
+      .select('country')
+      // filter out multiple returned results
+      .distinct()
+      .orderBy('country', 'ASC')
+      // display successful results. will return empty array if no results.
+      .then((rows) => {
+        res.status(200).json(rows)
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json({ "Error": true, "Message": err + "Error executing MySQL query." })
+      })
+  }
 })
 
+
+// function to test for user authorization status when attempting to access the factors end point.
 const authorize = (req, res, next) => {
+
   const authorization = req.headers.authorization;
   let token = null;
-
-  // const exampleToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1pa2VAZ21haWwuY29tIiwiZXhwIjoxNjIyNzIxMzAyLCJpYXQiOjE2MjI2MzQ5MDJ9.0tG9uRePqlehYSEhkJl1txfwUNBs1uYIicDjfA14k0Y";
+  const secretKey = "secret key";
 
   if (authorization && authorization.split(" ").length === 2) {
     token = authorization.split(" ")[1]
-    console.log("Tokens ", token)
   }
   else {
-    res.status(401).json({ error: true, message: "err + : Unauthorized" })
+    res.status(401).json({ error: true, message: "Authorization header ('Bearer token') not found" })
     return
   }
 
   // verify JWT Token
   try {
-    const decode = jwt.verify(token, secretKey)
+    const decoded = jwt.verify(token, secretKey)
 
     if (decoded.exp < Date.now()) {
-      res.status(401).json({ error: true, message: "Token Expired" })
+      res.status(401).json({ error: true, message: "JWT token has expired" })
       return
     }
 
     // permit user to advance
     next()
   } catch (e) {
-    res.status(401).json({ error: true, message: err + " : Unathorized" })
+    res.status(401).json({ error: true, message: "Invalid JWT token" })
   }
 }
 
+
 // factors page
-router.get("/factors/:year", authorize, function (req, res, next) {
+router.get("/factors/:year", authorize, function (req, res) {
   // catch the 404 error for navigating straight to factors with a year in the url
 
-  const optionalFactorParams = req.query;
+  const optionalFactParams = req.query;
+  const limit = req.query.limit;
+  const country = req.query.country;
   const year = req.params.year;
+
   var validYear = checkYearFormat(year);
+  var invalidParams = checkInvalidFactParams(optionalFactParams);
+  var validLimit = checkLimitFormat(limit);
+  var validCountry = checkCountryFormat(country);
 
   if (!validYear) {
-    res.status(400).json({ message: "Invalid year format. Format must be yyyy" });
-  } else {
+    res.status(400).json({ "Error": true, "Message": "Invalid year format. Format must be yyyy" });
+  }
+  else if (invalidParams) {
+    res.status(400).json({ "Error": true, "Message": "Invalid Query Parameters. Only limit and country are permitted." })
+    return
+  }
+  else if (limit && !validLimit) {
+    res.status(400).json({ "Error": true, "Message": "Invalid limit query. Limit must be a positive number." })
+  }
+  else if (country && validCountry) {
+    res.status(400).json({ "Error": true, "Message": "Invalid country format. Country query parameter cannot contain numbers." })
+  }
+  else {
     req.db.from('rankings').select('*').where('year', year)
-      .modify(withOptionalFactorParams, optionalFactorParams)
+      .modify(withOptionalFactorParams, optionalFactParams)
       .then((rows) => {
-        if (!rows.length) {
-          res.json({ "Error": false, "Message": "There are no matching records this query" })
-        } else {
-          res.json({ "Error": false, "Message": "Success", "Country Factors: ": rows })
-        }
-      })
+        res.json(rows)
+      }
+      )
       .catch((err) => {
-        console.log(err);
-        res.json({ "Error": true, "Message": "Error in  MySQL query" })
+        // console.log(err);
+        res.json({ "Error": true, "Message": err + "Error in  MySQL query" })
       })
   };
 
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const saltRounds = 10;
-
+const secretKey = "secret key";
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
@@ -11,9 +11,12 @@ router.get('/', function (req, res, next) {
 });
 
 router.post("/register", function (req, res, next) {
-  //1. retrieve  email and password from req.body
+
+  //retrieve email and password from req.body
   const email = req.body.email
   const password = req.body.password
+
+  // check to ensure that both values are defined
   if (!email || !password) {
     res.status(400).json({
       error: true,
@@ -21,13 +24,15 @@ router.post("/register", function (req, res, next) {
     })
     return
   }
-  //2. determine if the user already exists in the tables
+
+  //  determine if the user already exists in the tables
   const queryUsers = req.db.from("users").select("*").where("email", "=", email)
+
   queryUsers
-    //2.2 if user does exist, return error res  ponse
+    // if user does exist, return error response
     .then((users) => {
       if (users.length > 0) {
-        res.status(401).json({ error: true, message: "User already exists" }).
+        res.status(409).json({ error: true, message: "User already exists" }).
           return;
       }
       //2.1 if user does not exist, insert into table
@@ -35,51 +40,122 @@ router.post("/register", function (req, res, next) {
       return req.db.from("users").insert({ email, hash })
     })
     .then(() => {
-      res.status(201).json({ success: true, message: "User Created" })
+      res.status(201).json({ success: true, message: "User created" })
     })
 })
 
 router.post("/login", function (req, res, next) {
+
   const email = req.body.email
   const password = req.body.password
 
+  // check to ensure that both values are defined
   if (!email || !password) {
     res.status(400).json({
       error: true,
-      message: "Request body incomplete - email and password needed"
+      message: "Request body incomplete, both email and password are required"
     })
     return
   }
+  // check to see if 
   const queryUsers = req.db.from("users").select("*").where("email", "=", email)
   queryUsers.then((users) => {
+
+    // check if email currently exists in db, if not: throw error to inform user
     if (users.length === 0) {
-      res.status(401).json({ error: true, message: "User Does not Exist" }).
+      res.status(401).json({ error: true, message: "Incorrect email or password" }).
         return
     }
-    //comapare pw hashes
+
+    // if there is a matching email in db, comapare pw and user pw hash
     const user = users[0]
     return bcrypt.compare(password, user.hash)
   })
+    // if pw do not match, throw error to inform user
     .then((match) => {
       if (!match) {
-        res.status(401).json({ error: true, message: "Unauthorized" }).
+        res.status(401).json({ error: true, message: "Incorrect email or password" }).
           return
       }
+      // if pw matches, create and associate jwt and its expirey to user email
       const secretKey = "secret key"
       const expires_in = 60 * 60 * 24
       const exp = Date.now() + expires_in * 1000
       const token = jwt.sign({ email, exp }, secretKey)
-      res.json({ token_type: "Bearer", token, expires_in })
+      res.status(200).json({ token_type: "Bearer", token, expires_in })
     })
-})
+});
+
+const authorize = (req, res, next) => {
+
+  const authorization = req.headers.authorization;
+  let token = null;
 
 
+  if (authorization && authorization.split(" ").length === 2) {
+    token = authorization.split(" ")[1]
+  }
+  //  
 
-router.get("/:email/profile", function (req, res, next) {
+  // verify JWT Token
+  try {
+    const decoded = jwt.verify(token, secretKey)
 
-  const email = req.body.email;
-  
+    if (decoded.exp < Date.now()) {
+      res.status(401).json({ error: true, message: "JWT token has expired" })
+      return
+    }
 
-})
+    // permit user to advance
+    next()
+  } catch (e) {
+    res.status(401).json({ error: true, message: "Invalid JWT token" })
+  }
+};
+
+// get email profile 
+router.get("/:email/profile", authorize, function (req, res, next) {
+
+  const authorization = req.headers.authorization;
+
+  // capturing email from the users parameter input
+  const email = req.params.email;
+    
+
+  const decodedEmail = authorize.decoded.email;
+
+  // query for searching db for matching email address in user table
+  const queryUsers = req.db.from("users").select("*").where("email", "=", email)
+
+  const publicProfileQuery = req.db.from("users").select('email', 'firstName', 'lastName').where('email', email)
+
+  const authedProfileQuery = req.db.from("users").select('email', 'firstName', 'lastName', 'dob', 'address').where('email', email)
+
+  queryUsers.then((users) => {
+
+    // check to see if user email exists within db and throw error if none.
+    if (useriles.length === 0) {
+      res.status(401).json({ error: true, message: "User not found" }).
+        return
+    }
+
+    // check the token authorisation state and whether the email in the JWT and the email parameter entered match. If there is no token or the emails do not match, return the basic public profile information
+    else if (authorize.token === null || decodedEmail != email) {
+      publicProfileQuery.then((rows) => {
+        res.status(200).json(rows)
+      })
+    }
+
+    // if there is a JWT present and the JWT email and parameter email match, this confirms user authentication. if so, return the full set of user details 
+    else if (authorize.token != null && decodedEmail === email) {
+      authedProfileQuery.then((rows) => {
+        res.status(200).json(rows)
+      })
+        .catch((err) => {
+          res.status(400).json({ "Error": true, "Message": "Error executing MySQL query" })
+        })
+    }
+  })
+});
 
 module.exports = router;
